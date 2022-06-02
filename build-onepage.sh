d064441ffafdd88e795853297849698fb4c8ece9
Aykut A <aykut.acikel@gmail.com>
2022-06-02 16:17:26 +0200

    separate chapters in DevOps or everyday tools

--- build-onepage.sh
+++ build-onepage.sh
@@ -2,14 +2,20 @@
 
 # github doesn't support "include::" so I'm concatening all the adocs into a single one
 
-cat << EOF > unified.adoc
+cat << EOF > unified-devops.adoc
 = Docs
 :toc: left
 :sectanchors:
 :source-highlighter: highlight.js
 EOF
-
-cat chapters/*.adoc >> unified.adoc
+cat chapters/devops/*.adoc >> unified-devops.adoc
+cat << EOF > unified-tools.adoc
+= Docs
+:toc: left
+:sectanchors:
+:source-highlighter: highlight.js
+EOF
+cat chapters/tools/*.adoc >> unified-tools.adoc
 
 
 cat << EOF > rust.adoc
@@ -22,7 +28,8 @@ EOF
 cat rust/*.adoc >> rust.adoc
 
 
-asciidoctor unified.adoc
+asciidoctor unified-devops.adoc
+asciidoctor unified-tools.adoc
 asciidoctor rust.adoc
 
 asciidoctor readme.adoc
